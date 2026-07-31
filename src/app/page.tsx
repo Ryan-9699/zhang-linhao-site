75ff4753984a70f2f2157f9215fc890527ff5200
ryan64207579-pixel <ryan.64207579@gmail.com>
2026-07-31 18:14:11 +0800

    添加 Sites 外部托管配置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
-import { readFile } from "node:fs/promises";
-import path from "node:path";
+import content from "../../public/content.json";
 import HaylerInspiredSite from "@/components/HaylerInspiredSite";
 import type { ContentData } from "@/types/content";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-async function loadContent() {
-  const contentPath = path.join(process.cwd(), "public", "content.json");
-  return JSON.parse(await readFile(contentPath, "utf8")) as ContentData;
-}
-
-export default async function Home() {
-  const content = await loadContent();
-  return <HaylerInspiredSite initialContent={content} />;
+export default function Home() {
+  return <HaylerInspiredSite initialContent={content as ContentData} />;
 }
